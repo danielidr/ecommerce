ae61334dc288962d99d1dbae569cf4c34b83fd23
Danielid Ramirez <danielid132@gmail.com>
2021-08-01 23:49:47 -0400

    Adding category test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class CategoryTest < ActiveSupport::TestCase
     test "a category can only have one parent" do
       parent = Category.create(name: "bedroom")
-      Category.create(name: "pillows", subcategory_id: parent.id)
+      child = Category.create(name: "pillows", subcategory_id: parent.id)
+      assert child.subcategory_id == parent.id
+      assert Category.where(subcategory_id: parent.id).size == 1
     end
 end
