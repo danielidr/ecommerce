d5c756a71daa41bedc7c87aa10ae41e248362c54
Danielid Ramirez <danielid132@gmail.com>
2021-08-03 21:35:38 -0400

    Adding category methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,29 @@ class Category < ApplicationRecord
 
   has_many :subcategories, class_name: "Category", foreign_key: "subcategory_id", dependent: :destroy
   belongs_to :parent_category, class_name: "Category", foreign_key: "subcategory_id", optional: true
+
+  def get_parent
+    Category.find_by(id: subcategory_id)
+  end
+
+  def get_parents
+    parent = get_parent()
+    parents = [parent]
+    if parent != nil
+      p = parent.get_parent
+      while p != nil
+        parents.push(p)
+        p = p.get_parent
+      end
+      return parents 
+    end
+    return []
+  end
+
+  def descendents
+    subcategories.map do |subcategory|
+      [subcategory] + subcategory.descendents
+    end.flatten
+  end
+
 end
--- db/seeds/categories.rb
+++ db/seeds/categories.rb
@@ -1,5 +1,7 @@
 Category.create(name: "Tecnology")
 Category.create(name: "Smartphones", subcategory_id: 1)
-Category.create(name: "Shoes")
 Category.create(name: "Accesories")
+Category.create(name: "Shoes", subcategory_id: 3)
+Category.create(name: "Casual", subcategory_id: 4)
+
 
